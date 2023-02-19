3f28b0c4af3352ee07720563693b20c90580eac9
MariaAbdullah97 <meroo3066@gmail.com>
2023-02-19 15:28:14 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const form =document.getElementById('create-task-form');
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const task=document.getElementById('tasks');
+    const p=document.createElement('p');
+    const btn=document.createElement("button");
+    const input=document.getElementById('new-task-description');
+    p.textContent=e.target['new-task-description'].value
+    btn.textContent="x";
+    btn.addEventListener('click',()=>{
+      btn.remove();
+      p.remove();
+    })
+    task.appendChild(p);
+    p.appendChild(btn);
+    
+
+    
+
+  })
   // your code here
 });
+
+
